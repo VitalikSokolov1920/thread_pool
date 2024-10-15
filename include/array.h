fabe2ea5772dd0e89466d9b67273d6bb94a3612e
vitalik <sokolov2048@mail.ru>
2024-10-15 22:15:22 +0300

    update array.h

--- include/array.h
+++ include/array.h
@@ -1,11 +1,18 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
-typedef unsigned int array_len_t;
+#include <stddef.h>
 
 typedef struct {
     void* elems;
-    array_len_t len;
+    size_t elem_size;
+    size_t len;
 } array_t;
+
+array_t array_init(size_t size, size_t elem_size);
+
+array_t* array_add(array_t* array, void* elem);
+
+array_t* array_remove(array_t* array, size_t idx);
 
 #endif
